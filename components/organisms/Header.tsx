ebd72610251a87ea5d58022d054f034c695db575
BeZeelBeast <157466868+BeZeelBeast@users.noreply.github.com>
2025-08-09 01:07:10 +0700

    edit

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <nav className="p-4 bg-cyan-500 flex flex-col sm:flex-row sm:justify-between items-center gap-3">
-      <div className="font-mono font-bold text-xl">CariFilm</div>
+      <div className="font-mono font-bold text-xl">ListFilm</div>
 
       <ul className="flex gap-6 font-mono font-bold">
         <li><Link href="/">Home</Link></li>
